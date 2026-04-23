8f181005a21ca913019c488fac0c3bbe97477747
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2026-04-23 12:57:55 +0530

    feat: enhance user registration with validation and check for existing users; integrate file upload middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,40 @@
 import { asyncHandler } from "../utils/asynchandler.js";
+import{ ApiError } from "../utils/ApiError.js";
+
+import {User} from "../models/User.model.js"
 
 //Kind of Higher order function that accepts a function
 const registerUser = asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message:"Chai aur Code"
-    })
-})
+    // get user details from frontend/ postman
+    // validation - not empty, email format, password strength
+    // check if user already exists in database using username or email
+    // check for images, check for avatar
+    // upload them to cloudinary and get the url from response, avatar
+    // create user object - create entry in database
+    // remove password and refresh token from response
+    // check for user creation
+    // return res
 
+
+
+    // if data coming from form or in json format we use req.body
+    // if url its diff approach
+    const {fullName,email,username,password} = req.body
+    console.log("User registered email:", email);
+
+    // Instead of creating multiple if conditions for each field, we can use array and "some" method to check if any field is empty or contains only whitespace
+    if(
+        [fullName,email,username,password].some((field)=> field?.trim() === '')
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or:[{ username },{ email }]
+    })
+
+    if(existedUser){
+        throw new ApiError(400, "User already exists with this email or username")
+    }
+})
 export { registerUser }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,25 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
+import {uploader} from "../middlewares/multer.js"
+
 const router = Router()
 
-router.route('/register').post(registerUser) //http://localhost:8000/api/v1/users/register
+router.route('/register').post(
+
+    // middleware injection for file upload
+    upload.fields([ 
+        {
+            name:'avatar',
+            maxCount:1
+        },
+        {
+            name:'coverImage',
+            maxCount:1
+        }
+    ]),
+
+    registerUser,
+) //http://localhost:8000/api/v1/users/register
 
 export default router;
