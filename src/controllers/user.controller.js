4ae456a9f93a74dbcb22b6365b21746150d256f9
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2026-04-27 16:13:58 +0530

    Added getWatchHistory in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -520,17 +520,75 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         }
     ])
 
-    if (!channel?.length){
-        throw new ApiError(404,'Channel does not exist')
+    if (!channel?.length) {
+        throw new ApiError(404, 'Channel does not exist')
     }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                channel[0],
+                'User channel fetched successfully'
+            )
+        )
+
+})
+
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'Videos',
+                localField: 'watchHistory',
+                foreignField: '_id',
+                as: 'watchHistory',
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: 'users',
+                            localField: 'owner',
+                            foreignField: '_id',
+                            as: 'owner',
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: '$owner'
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+
+    ])
 
     return res
     .status(200)
     .json(
         new ApiResponse(
             200,
-            channel[0],
-            'User channel fetched successfully'
+            user[0].watchHistory,
+            'Watch history fetched successfully'
         )
     )
 
@@ -547,6 +605,7 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 
 }
